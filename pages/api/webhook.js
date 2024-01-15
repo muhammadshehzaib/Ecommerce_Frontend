43a42ec733d3c922d558d6a7ef6d77ad87bb9d0e
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-01-15 14:37:09 +0500

    ecommerce frontend deployed

---
+++ pages/api/webhook.js
@@ -0,0 +1,51 @@
+import { mongooseConnect } from "@/lib/mongoose";
+const stripe = require("stripe")(process.env.STRIPE_SK);
+import { buffer } from "micro";
+import { Order } from "@/models/Order";
+
+const endpointSecret =
+  "whsec_dd481e04332ffeb7a7244a16fd65f5998de0272378ee0faeacda55f72f3ab7b6";
+
+export default async function handler(req, res) {
+  await mongooseConnect();
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await buffer(req),
+      sig,
+      endpointSecret
+    );
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "payment_intent.succeeded":
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === "paid";
+      console.log(paid);
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId, {
+          paid: true,
+        });
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send("ok");
+}
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+// bright-thrift-cajole-lean
+// acct_1Lj5ADIUXXMmgk2a
